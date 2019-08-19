174706b7521d8c036a93e36a5a0b30fb5aebd155
Marcel Tanner <marcel.tanner@uzh.ch>
2019-08-19 18:23:15 +0200

    feedback section order

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -157,12 +157,12 @@
       </div>
     </app-content-section>
 
+
+    <section-feedback email="wiesel@citizenscience.ch"></section-feedback>
+
     <section-s-d-g goal="15" color="light-greyish">
       This project is supporting goal no. 15 from the UN Sustainable Development Goals when an unknown printer took a galley of type and scrambled it to make a type specimen book.
     </section-s-d-g>
-
-
-    <section-feedback email="wiesel@citizenscience.ch"></section-feedback>
 
 
     <section-newsletter-signup></section-newsletter-signup>
